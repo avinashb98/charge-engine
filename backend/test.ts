df83e880ca656dda812b85a7357b4197d7516353
Avinash Bhardwaj <avinashb97@gmail.com>
2023-10-27 00:12:28 +0530

    add tests to validate changes

--- backend/test.ts
+++ backend/test.ts
@@ -8,16 +8,24 @@ async function test() {
     async function basicLatencyTest() {
         await app.post("/reset").expect(204);
         const start = performance.now();
-        await app.post("/charge").expect(200);
-        await app.post("/charge").expect(200);
-        await app.post("/charge").expect(200);
-        await app.post("/charge").expect(200);
-        await app.post("/charge").expect(200);
+        await app.post("/charge").send({ account: "test", charges: 1 }).expect(200);
+        await app.post("/charge").send({ account: "test", charges: 1 }).expect(200);
+        await app.post("/charge").send({ account: "test", charges: 1 }).expect(200);
+        await app.post("/charge").send({ account: "test", charges: 1 }).expect(200);
+        await app.post("/charge").send({ account: "test", charges: 1 }).expect(200);
         console.log(`Latency: ${performance.now() - start} ms`);
     }
 
+    async function basicValidationTests() {
+        await app.post("/charge").send({ account: "test" }).expect(400);
+        await app.post("/charge").send({ account: "test", charges: "a" }).expect(400);
+        await app.post("/charge").send({ account: "test", charges: "one" }).expect(400);
+        await app.post("/charge").send({ account: "test", charges: null }).expect(400);
+    }
+
     async function runTests() {
-        await basicLatencyTest();
+        // await basicLatencyTest();
+        await basicValidationTests();
     }
 
     runTests().catch(console.error);
